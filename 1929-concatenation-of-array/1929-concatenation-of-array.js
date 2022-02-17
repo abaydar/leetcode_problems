8dde48b29035c2c8fbee467c40cce21500c20d54
Amanda Baydar <amandabaydar@gmail.com>
2022-02-16 21:43:46 -0500

    Time: 109 ms (54.08%), Space: 45.2 MB (25.58%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let nums2 = nums
+    let ans = nums2.concat(nums)
+    return ans
+    
+};
